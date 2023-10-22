a7b2271bd5ee49c43bf2fba1dd81a7026ad60e18
m1rageLA <mirageLa@outlook.com>
2023-10-22 17:32:49 +0200

    Add task deletion

--- scripts.js
+++ scripts.js
@@ -2,7 +2,10 @@ const textInput = document.getElementById("nameField");
 const descrInput = document.getElementById("descrField");
 const taskList = document.getElementById("taskList");
 
-function createTaskElement(text, description) {
+let tasksCollection = [];
+let id = -1;
+
+function createTaskElement(text, description, currentID) {
     let task_li = document.createElement("li");
     let task_div = document.createElement("div");
     let task_radio = document.createElement("input");
@@ -11,42 +14,46 @@ function createTaskElement(text, description) {
     
     task_radio.setAttribute("type", "radio");
 
+    task_radio.addEventListener('click', () => {
+        removeTask(currentID, task_li);
+    });
+
     task_p_text.textContent = text;
     task_p_descr.textContent = description;
     
     task_li.classList.add('asd');
     task_div.classList.add('task-div');
     task_p_text.classList.add('a');
-    task_p_descr.classList.add('a');
 
     task_div.appendChild(task_radio);
     task_div.appendChild(task_p_text);
-    task_div.appendChild(task_p_descr);
     task_li.appendChild(task_div);
 
     return task_li;
 }
 
-let tasksCollection = [];
-let id = 0;
-
-function addTask() {
-    if (textInput.value !== "") {
-        let task = {
-            id: id++,
-            text: textInput.value,
-            completed: false,
-        };
-        tasksCollection.push(task);
-    } else {
-        console.log("Пустое поле");
-        return 0;
+    function addTask() {
+        if (textInput.value !== "") {
+            let task = {
+                id: id++,
+                text: textInput.value,
+                completed: false,
+            };
+            tasksCollection.push(task);
+        } else {
+            console.log("Пустое поле");
+            return 0;
+        }
     }
+
+function removeTask(currentID, task_li) {
+    task_li.remove();
+    tasksCollection[currentID].completed = true;
+    console.log(tasksCollection[currentID]);
 }
 
 document.getElementById("sendButton").addEventListener("click", () => {
     addTask();
-    const newTask = createTaskElement(tasksCollection[id - 1].text, tasksCollection[id - 1].description);
-    taskList.appendChild(newTask);
+    const { text, description, currentID = id } = tasksCollection[id];//--destructuring--
+    taskList.appendChild(createTaskElement(text, description, currentID));
 });
-
