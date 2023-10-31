a49376f4366701aa40968115df671e9cf44eb11e
m1rage <miragela.code@gmail.com>
2023-10-31 13:25:23 +0100

    Add completed tasks

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
 const textInput = document.getElementById("nameField");
 const descrInput = document.getElementById("descrField");
 const taskList = document.getElementById("taskList");
+const finishedList = document.getElementById("finishedTaskList");
 
 let tasksCollection = [];
+
 let id = parseInt(localStorage.getItem('taskID')) || 0;
 
 function createTaskElement(text, currentID) {
@@ -14,14 +16,15 @@ function createTaskElement(text, currentID) {
     task_radio.setAttribute("type", "radio");
 
     task_radio.addEventListener('click', () => {
-        removeTask(currentID, task_li);
+        removeTask(currentID, task_li, text);
     });
 
     task_p_text.textContent = text;
     
-    task_li.classList.add('asd');
+    task_li.classList.add('liElement');
     task_div.classList.add('task-div');
     task_p_text.classList.add('a');
+    task_radio.classList.add('radio');
 
     task_div.appendChild(task_radio);
     task_div.appendChild(task_p_text);
@@ -36,9 +39,7 @@ function addTask() {
             text: textInput.value,
             completed: false,
         };
-        
         id++;
-
         localStorage.setItem('taskID', id);
         tasksCollection.push(task);
         saveTasks(tasksCollection);
@@ -48,22 +49,22 @@ function addTask() {
     }
 }
 
-function removeTask(currentID, task_li) {
+function removeTask(currentID, task_li, text) {
     task_li.remove();
     tasksCollection[currentID].completed = true;
     saveTasks(tasksCollection);
+    finishedList.appendChild(createTaskElement(text, currentID));
 }
 
-document.getElementById("sendButton").addEventListener("click", () => {
+function performAction() {
     if (textInput.value !== "") {
         addTask();
         const currentID = id - 1;
         taskList.appendChild(createTaskElement(tasksCollection[currentID].text, currentID));
-    }
-    else {
+    } else {
         console.log("Пустое поле");
     }
-});
+}
 
 function saveTasks(tasks) {
     localStorage.setItem('tasks', JSON.stringify(tasks));    
@@ -80,6 +81,17 @@ tasksCollection.forEach((task, index) => {
     if (task.completed === false) {
         taskList.appendChild(createTaskElement(task.text, index));
     }
+    else if (task.completed === true){
+        finishedList.appendChild(createTaskElement(task.text, index));
+    }
 });
 
-//TODO: Сделать функциональность сделанных задач
+document.getElementById("sendButton").addEventListener("click", () => {
+    performAction();
+});
+
+textInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        performAction();
+    }
+});
